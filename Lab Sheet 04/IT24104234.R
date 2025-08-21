7794252deb9afe4e30d4d05003a8614e51f8a3b0
IT24104234 <IT24104234@my.sliit.lk>
2025-08-21 16:03:29 +0530

    Lab 4 Uploaded

---
+++ Lab Sheet 04/IT24104234.R
@@ -0,0 +1,43 @@
+setwd("C:\\Users\\it24104234\\Desktop\\IT24104234")
+
+# Import dataset
+branch_data <- read.table("Exercise.txt", header = TRUE)
+
+##Question 01
+branch_data <- read.table("Exercise.txt", header = TRUE, sep = ",")
+
+fix(branch_data)
+
+attach(branch_data)
+
+##Question 02
+str(branch_data)
+
+sapply(branch_data, class)
+
+##Question 03
+boxplot(Sales_X1,main="Boxplot for sales distribution",xlab="Sales",outline=TRUE,outpch=8,horizontal=TRUE)
+
+##Question 04
+quantile(Advertising_X2)
+
+IQR(Advertising_X2)
+
+##Question 05s
+find_outliers <- function(x) {
+  Q1 <- quantile(x, 0.25)
+  Q3 <- quantile(x, 0.75)
+  IQR <- Q3 - Q1
+  
+  lower_bound <- Q1 - 1.5 * IQR
+  upper_bound <- Q3 + 1.5 * IQR
+  
+  outliers <- x[x < lower_bound | x > upper_bound]
+  
+  return(outliers)
+}
+
+find_outliers(Years_X3)
+
+
+
